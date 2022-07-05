b9f43072a09ea544b237cf709b819945c6eb756b
satyaprakash <satyaprakashdcool@gmail.com>
2022-07-06 00:00:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ComponentInput from './component/Component';
+
+function App() {
+  return (
+    <div className="App">
+        <ComponentInput/>
+    </div>
+  );
+}
+
+export default App;
